8771321ab3f856c5556e8350af69be04267626de
VishwanathSinghYadav <singhvishwanathyadav@gmail.com>
2022-02-21 21:55:34 +0530

    added dynamic aspects.

--- src/App.js
+++ src/App.js
@@ -14,5 +14,25 @@ function App() {
   );
 }
 
+const books=[
+  {id: 1,
+    title: "Da Vinci Code",
+    edition: 2013,
+    author: "Dan Brown",
+    genre: "Mystery, Detective fiction, Conspiracy fiction, Thriller",
+    pages: '489',
+    theme: 'The Da Vinci Code follows "symbologist" Robert Langdon and cryptologist Sophie Neveu after a murder in the Louvre Museum in Paris causes them to become involved in a battle between the Priory of Sion and Opus Dei over the possibility of Jesus Christ and Mary Magdalene having had a child together.'}
+    ,
+    {id: 1,
+      title: "Harry Potter",
+      edition: 7(1997-2007),
+      author: "J. K. Rowling",
+      genre: "Fantasy",
+      pages: '223, 251, 317, 636, 766, 607, 607',
+      theme: 'According to Rowling, the main theme is death. Other major themes in the series include prejudice, corruption, and madness.'}
+      
+
+]
+
 export default App;
 
--- src/components/Book.js
+++ src/components/Book.js
@@ -11,28 +11,24 @@ export default function Book() {
             </div>
         </div>
         <div>
-            <span>Edition</span>
-            <span>11</span>
+            <span>Edition: </span>
+            <span>7(1997-2007)</span>
         </div>
         <div>
-            <span>Read Time</span>
-            <span>2 hrs</span>
+            <span>Author: </span>
+            <span>J. K. Rowling</span>
         </div>
         <div>
-            <span>Vol</span>
-            <span>8</span>
+            <span>Genre: </span>
+            <span>Fantasy</span>
         </div>
         <div>
-            <span>Pages</span>
-            <span>500</span>
+            <span>Pages: </span>
+            <span>223, 251, 317, 636, 766, 607, 607</span>
         </div>
         <div>
-            <span>Theme</span>
-            <span>QWERTYUIOPASDFGHJKLZXCVBNM</span>
-        </div>
-        <div>
-            <span>Read Time</span>
-            <span>2 hrs</span>
+            <span>Theme: </span>
+            <span>According to Rowling, the main theme is death. Other major themes in the series include prejudice, corruption, and madness.</span>
         </div>
     </div>
   )
